2b5f6d33e8ade91d8747e874fe00a66826ade9d1
Ogheneovo Grant-Oyeye <ogheneovo.grant-oyeye@mail.mcgill.ca>
2022-03-10 12:17:35 -0500

    cdcsd

--- db.js
+++ db.js
@@ -1,11 +1,5 @@
 const {Pool , Client} = require('pg')
-
-const pool = new Pool({
-    user: process.env.PGUSER,
-    host: process.env.PGHOST,
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT,
-  })
+const conn = process.env.DATABASE_URL
+const pool = new Pool({conn})
   
 module.exports = pool
